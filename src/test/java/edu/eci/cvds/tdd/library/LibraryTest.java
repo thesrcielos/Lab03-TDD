1653f876bf8cdb5f31608b786cc7f1693b194f71
unknown <diego.macia-d@labinfo.is.escuelaing.edu.co>
2024-08-31 11:16:50 -0500

    prueba fallida tdd

--- src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
+++ src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
@@ -1,4 +1,16 @@
 package edu.eci.cvds.tdd.library;
 
+import edu.eci.cvds.tdd.library.book.Book;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class LibraryTest {
+
+    @Test
+    public void addBookTest(){
+        Book book = new Book("example title","pepito ","1717263847194");
+        Library library = new Library();
+        assertTrue(library.addBook(book));
+    }
 }
